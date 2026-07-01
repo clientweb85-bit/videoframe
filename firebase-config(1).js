947a40f241f4cba02d2976b03a359032e16dab00
clientweb85-bit <clientweb85@gmail.com>
2026-07-01 19:25:53 +0530

    Add files via upload

---
+++ firebase-config(1).js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGkXXpUo9BI0ec6UzRTExOKQy8hRFcpmY",
+  authDomain: "videoframe-4bb13.firebaseapp.com",
+  databaseURL: "https://videoframe-4bb13-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "videoframe-4bb13",
+  storageBucket: "videoframe-4bb13.firebasestorage.app",
+  messagingSenderId: "981528362246",
+  appId: "1:981528362246:web:8f23961d012c504fa2b287",
+  measurementId: "G-JWKG06R0XT"
+};
+
+const IMGBB_API_KEY = "8dc461977590f92fa999d6952c2a34af";
+const CLOUDINARY_CLOUD = "thega6yv";
+const CLOUDINARY_PRESET = "videoframe1";
